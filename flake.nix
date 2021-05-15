068871a24ebdfcb6c53b5281278ec627a36bfee7
Ian Macalinao <github@igm.pub>
2021-05-15 17:06:55 -0500

    Lock to nixpkgs 20.09 release

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Nix overlay for StableSwap projects.";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-20.09";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:

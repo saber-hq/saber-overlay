bca3b08f8e871c64d94266572b42fd499f466e46
Ian Macalinao <ian@stableswap.pro>
2021-06-07 18:43:23 -0500

    temporarily hide anchor

--- env.nix
+++ env.nix
@@ -1,5 +1,11 @@
 { pkgs }:
 pkgs.buildEnv {
   name = "stableswap-env";
-  paths = with pkgs.stableswap; [ solana anchor rust spl-token-cli ];
+  paths = with pkgs.stableswap; [
+    solana
+    # TODO(igm): add anchor back in once it no longer uses a git dep
+    # anchor 
+    rust
+    spl-token-cli
+  ];
 }

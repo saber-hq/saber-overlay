cd66f737b15c3bfaa2d7cb899db14d29a6fd039d
Ian Macalinao <ian@saber.so>
2021-06-10 12:13:39 -0500

    Use nightly rust

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 final: prev:
 let
-  rust = prev.rust-bin.stable.latest.default;
+  rust = prev.rust-bin.nightly."2021-06-09".default;
   rustPlatform = prev.recurseIntoAttrs (prev.makeRustPlatform {
     rustc = rust;
     cargo = rust;

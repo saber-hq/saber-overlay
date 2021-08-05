e63d9a0ef818519fb6e298be854d9a57c8c2fee8
Ian Macalinao <ian@saber.so>
2021-08-05 17:57:36 -0500

    Update to Anchor 0.12.0 (#10)

--- packages/anchor/default.nix
+++ packages/anchor/default.nix
@@ -2,20 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "anchor";
-  version = "0.11.1";
+  version = "0.12.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "project-serum";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-ZwYv3XZujMCcQUgd1AHn0xNE1TMcakm5PNsNrGe8LVU=";
+    sha256 = "sha256-87Q/mPF/7sLDiT9N1MqjEX5E3dsLJ+oO2iyx9RcsK1g=";
   };
 
-  cargoPatches = [
-    # a patch file to add/update Cargo.lock in the source code
-    ./fix-Cargo.lock.patch
-  ];
-  cargoSha256 = "sha256-KNyb26+ZyNe9latyN5AlqglORQ0ZVLpzs7s7vhlXxc8=";
+  # cargoPatches = [
+  #   # a patch file to add/update Cargo.lock in the source code
+  #   ./fix-Cargo.lock.patch
+  # ];
+  cargoSha256 = "sha256-iaapemx8ZTdeDvDHfCxtrwUlo2U+Sr96EWWODeIMU1w=";
   verifyCargoDeps = false;
 
   nativeBuildInputs = with pkgs; [ pkgconfig ];
